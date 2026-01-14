966103ac21623f0d383ea856275b16a0686a64af
Nilssoni <martikainen93@gmail.com>
2026-01-14 17:18:34 +0200

    edited file a bit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { add, division } from "./calculator.js";
 const addResult = add(2, 3);
 console.log(addResult);
 
-const divResult = division(10,2);
+const divResult = division(9,2);
 console.log(divResult);
